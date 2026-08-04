35e0e164def12a2e702d0f7a28b3e7fe8b908317
hwnkue10-arch <hwnkue10@gmail.com>
2026-08-04 16:29:51 +0900

    Update book.py

--- 0804/LibrarySystem_Python/book.py
+++ 0804/LibrarySystem_Python/book.py
@@ -1,66 +1,128 @@
 class Book:
 
-    def __init__(self, title, author, publisher, borrowed=False):
+
+    def __init__(
+        self,
+        title,
+        author,
+        publisher,
+        borrowed=False
+    ):
+
         self.title = title
         self.author = author
         self.publisher = publisher
         self.borrowed = borrowed
 
 
-    # 도서 정보 출력
-    def show_info(self):
 
-        print("------------------------")
-        print("제목 :", self.title)
-        print("저자 :", self.author)
-        print("출판사 :", self.publisher)
+    # GUI 표시용 도서 정보 반환
 
-        if self.borrowed:
-            print("상태 : 대출중")
-        else:
-            print("상태 : 대출가능")
+    def get_info(self):
 
-        print("------------------------")
+        status = (
+            "대출중"
+            if self.borrowed
+            else "대출가능"
+        )
+
+
+        return (
+
+            self.title,
+
+            self.author,
+
+            self.publisher,
+
+            status
+
+        )
+
 
 
     # 대출
+
     def borrow(self):
 
+
         if self.borrowed:
+
             return False
 
+
+
         self.borrowed = True
+
         return True
+
+
+
 
 
     # 반납
+
     def return_book(self):
 
+
         if not self.borrowed:
+
             return False
 
+
+
         self.borrowed = False
+
         return True
 
 
-    # JSON 저장용 데이터 변환
+
+
+
+    # JSON 저장용 변환
+
     def to_dict(self):
 
+
         return {
-            "title": self.title,
-            "author": self.author,
-            "publisher": self.publisher,
-            "borrowed": self.borrowed
+
+
+            "title":
+                self.title,
+
+
+            "author":
+                self.author,
+
+
+            "publisher":
+                self.publisher,
+
+
+            "borrowed":
+                self.borrowed
+
         }
 
 
+
+
+
     # JSON 데이터를 Book 객체로 변환
+
     @staticmethod
+
     def from_dict(data):
 
+
         return Book(
+
             data["title"],
+
             data["author"],
+
             data["publisher"],
+
             data["borrowed"]
+
         )
